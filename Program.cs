fc22a19a2563e541671a8702be303732862646d8
HAROLD ADRIAN [mill5] <haroldb@mill5.com>
2022-01-20 08:24:49 -0500

    scope: reimplement a minor features about names and namespace

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace submodule02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("+------------------------------------------+");
+			Console.WriteLine("| Program: submodule02                     |");
+			Console.WriteLine("+------------------------------------------+");
+			Console.WriteLine("| C# Program by Harold Adrian Bolaños R    |");
+			Console.WriteLine("| This is a .Net Pilot for rollout CI/CD   |");
+			Console.WriteLine("| Program made in linux ubuntu             |");
+			Console.WriteLine("+------------------------------------------+");
+			Console.WriteLine("|                 History                  |");
+			Console.WriteLine("| 20220120 < initial rollout               |");
+			Console.WriteLine("+------------------------------------------+");
+        }
+    }
+}
